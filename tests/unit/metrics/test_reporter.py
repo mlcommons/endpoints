#15: Track first chunks and fix TPOT calculation (#23)
* Track first chunk and remove first chunk tokens from TPOT calc

* Simplify non-first-token tracking, fix example 01

* Fix msgspec types for first-chunk logging support

* Add error logging to EventRecorder

* Add error_count to MetricsReporter

* Add TPS to metrics report

* Simplify response_output construction, add some comments to explain certain datastructures

* Rename read_output_rows to iter_output_rows to indicate it is an Iterator

---------

Co-authored-by: Zhihan Jiang <68881590+nvzhihanj@users.noreply.github.com>

tests/unit/metrics/test_reporter.py
@@ -26,9 +26,16 @@ from inference_endpoint.metrics.reporter import (
 )
 
 
-class CharacterTokenizer:
-    def tokenize(self, text: str) -> list[str]:
-        return list(text)
+def test_sample_counting(events_db):
+    with MetricsReporter(events_db) as reporter:
+        stats = reporter.get_sample_statuses()
+        assert stats["completed"] == 2
+        assert stats["in_flight"] == 1
+
+
+def test_error_counting(events_db):
+    with MetricsReporter(events_db) as reporter:
+        assert reporter.get_error_count() == 3
 
 
 def test_derive_ttft(events_db, sample_uuids):
@@ -46,24 +53,24 @@ def test_derive_ttft(events_db, sample_uuids):
     assert ttft_rows.filter_uuid("asdf", only_first=False) == ()
 
 
-def test_derive_tpot(events_db, sample_uuids, fake_outputs):
+def test_derive_tpot(events_db, sample_uuids, fake_outputs, tokenizer):
     uuid1 = sample_uuids(1)
     uuid2 = sample_uuids(2)
 
     with MetricsReporter(events_db) as reporter:
         reporter.outputs_path = fake_outputs.path
         tpot_rows = reporter.derive_TPOT(
-            CharacterTokenizer(), reporting_mode=TPOTReportingMode.TOKEN_WEIGHTED
+            tokenizer, reporting_mode=TPOTReportingMode.TOKEN_WEIGHTED
         )
 
     # From test_derive_sample_latency and ttft:
-    expected_tpot1 = (10211 - 10000 - 10) / (len(fake_outputs[uuid1]) - 1)
-    expected_tpot2 = (10219 - 10003 - 187) / (len(fake_outputs[uuid2]) - 1)
+    expected_tpot1 = (10211 - 10000 - 10) / len(fake_outputs[uuid1][1])
+    expected_tpot2 = (10219 - 10003 - 187) / len(fake_outputs[uuid2][1])
 
     tpot1 = tpot_rows.filter_uuid(uuid1, only_first=False)
     tpot2 = tpot_rows.filter_uuid(uuid2, only_first=False)
-    assert len(tpot1) == len(fake_outputs[uuid1]) - 1
-    assert len(tpot2) == len(fake_outputs[uuid2]) - 1
+    assert len(tpot1) == len(fake_outputs[uuid1][1])
+    assert len(tpot2) == len(fake_outputs[uuid2][1])
     assert all(tpot == expected_tpot1 for tpot in tpot1)
     assert all(tpot == expected_tpot2 for tpot in tpot2)
 
@@ -116,31 +123,44 @@ def test_derive_duration_malformed(tmp_path):
 
 
 def test_tpot_to_histogram(
-    events_db_reporter_with_fake_outputs, fake_outputs, sample_uuids
+    events_db_reporter_with_fake_outputs, fake_outputs, tokenizer, sample_uuids
 ):
     uuid1 = sample_uuids(1)
     uuid2 = sample_uuids(2)
 
-    expected_tpot1 = (10211 - 10000 - 10) / (len(fake_outputs[uuid1]) - 1)
-    expected_tpot2 = (10219 - 10003 - 187) / (len(fake_outputs[uuid2]) - 1)
-    smaller, larger = sorted([expected_tpot1, expected_tpot2])
+    expected = [
+        {
+            "tpot": (10211 - 10000 - 10) / len(fake_outputs[uuid1][1]),
+            "count": len(fake_outputs[uuid1][1]),
+        },
+        {
+            "tpot": (10219 - 10003 - 187) / len(fake_outputs[uuid2][1]),
+            "count": len(fake_outputs[uuid2][1]),
+        },
+    ]
+    expected.sort(key=lambda x: x["tpot"])
+
+    bucket_boundaries = [
+        expected[0]["tpot"] - 1,
+        (expected[0]["tpot"] + expected[1]["tpot"]) / 2,
+        expected[1]["tpot"] + 1,
+    ]
 
     reporter = events_db_reporter_with_fake_outputs
     tpot_rows = reporter.derive_TPOT(
-        CharacterTokenizer(), reporting_mode=TPOTReportingMode.TOKEN_WEIGHTED
+        tokenizer, reporting_mode=TPOTReportingMode.TOKEN_WEIGHTED
     )
 
     # This isn't documented since it's an internal detail and should not be relied on, but `n_buckets`
     # is passed directly to np.histogram, so we can specify exact buckets to use
-    bucket_boundaries = [smaller - 1, (smaller + larger) / 2, larger + 1]
     buckets, counts = tpot_rows.to_histogram(n_buckets=bucket_boundaries)
     assert len(buckets) == 2
     assert len(counts) == 2
 
     assert buckets[0] == (bucket_boundaries[0], bucket_boundaries[1])
     assert buckets[1] == (bucket_boundaries[1], bucket_boundaries[2])
-    assert counts[0] == len(fake_outputs[uuid1]) - 1
-    assert counts[1] == len(fake_outputs[uuid2]) - 1
+    assert counts[0] == expected[0]["count"]
+    assert counts[1] == expected[1]["count"]
 
 
 def test_percentile():
@@ -184,9 +204,10 @@ def test_rollup_summarize(events_db):
         assert summary["percentiles"][s] == latencies.percentile(percentile)
 
 
-def test_reporter_create_report(events_db_reporter_with_fake_outputs):
+def test_reporter_create_report(
+    events_db_reporter_with_fake_outputs, fake_outputs, tokenizer
+):
     reporter = events_db_reporter_with_fake_outputs
-    tokenizer = CharacterTokenizer()
 
     report = reporter.create_report(tokenizer)
 
@@ -220,6 +241,14 @@ def test_reporter_create_report(events_db_reporter_with_fake_outputs):
     # QPS should be: completed_samples / (duration_ns / 1e9)
     expected_qps = report.n_samples_completed / (report.duration_ns / 1e9)
     assert report.qps == expected_qps
+
+    expected_total_tokens = 0
+    for output in fake_outputs.values():
+        for chunk in output:
+            expected_total_tokens += len(tokenizer.tokenize(chunk))
+    expected_tps = expected_total_tokens / ((10300 - 5000) / 1e9)
+    assert report.tps == expected_tps
+
     assert report.e2e_sample_latency_sec == expected_e2e_latency_ns / 1e9
 
 
@@ -241,6 +270,7 @@ def test_reporter_json(events_db):
         "output_sequence_lengths",
         "tpot_reporting_mode",
         "qps",
+        "tps",
         "e2e_sample_latency_sec",
     ]
     assert set(json_dict.keys()) == set(expected_keys)
@@ -248,6 +278,7 @@ def test_reporter_json(events_db):
     assert json_dict["n_samples_completed"] == report.n_samples_completed
     assert json_dict["duration_ns"] == report.duration_ns
     assert json_dict["qps"] == report.qps
+    assert json_dict["tps"] == report.tps
     assert json_dict["e2e_sample_latency_sec"] == report.e2e_sample_latency_sec
 
     # For ttft, tpot, and latency, JSON decode will only decode as lists, not tuples